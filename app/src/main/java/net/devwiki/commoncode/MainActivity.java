e4909db957362ad2f2db14075a69749fc0ee1345
DevWiki <674952098@qq.com>
2016-08-15 21:19:50 +0800

    网络工具测试

--- app/src/main/java/net/devwiki/commoncode/MainActivity.java
+++ app/src/main/java/net/devwiki/commoncode/MainActivity.java
@@ -1,13 +1,34 @@
 package net.devwiki.commoncode;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.network_btn)
+    Button networkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R.id.network_btn})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.network_btn:
+                startActivity(new Intent(this, NetworkActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/net/devwiki/commoncode/NetworkActivity.java
@@ -0,0 +1,30 @@
+package net.devwiki.commoncode;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+
+import net.devwiki.common.util.NetworkUtil;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class NetworkActivity extends AppCompatActivity {
+
+    @BindView(R.id.open_btn)
+    Button openBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.open_btn)
+    public void onClick() {
+        NetworkUtil.openNetworkSettings(getApplicationContext());
+    }
+}
