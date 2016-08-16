19560a0e268f743006e6111efa50590f3bae4208
Asia <bozhou2010@live.com>
2016-08-16 23:34:38 +0800

    添加文件工具类

--- app/src/main/java/net/devwiki/commoncode/FileActivity.java
+++ app/src/main/java/net/devwiki/commoncode/FileActivity.java
@@ -1,15 +1,28 @@
 package net.devwiki.commoncode;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import net.devwiki.common.util.FileUtil;
+import net.devwiki.common.util.ToastUtil;
+import net.devwiki.log.DevLog;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class FileActivity extends AppCompatActivity {
+
+    private static final int CODE_REQUIRE_PEMISSION = 0x001;
 
     @BindView(R.id.file_tv)
     TextView fileTv;
@@ -19,5 +32,37 @@ public class FileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file);
         ButterKnife.bind(this);
+
+        checkPermission();
+
+        DevLog.d(File.separator);
+        DevLog.d(File.pathSeparator);
+        DevLog.d("root:" + Environment.getRootDirectory().getAbsolutePath());
+        DevLog.d("state:" + Environment.getExternalStorageState());
+        DevLog.d("external:" + Environment.getExternalStorageDirectory().getAbsolutePath());
+        DevLog.d("data:" + Environment.getDataDirectory().getAbsolutePath());
+        DevLog.d("download cache:" + Environment.getDownloadCacheDirectory().getAbsolutePath());
+        DevLog.d("music:" + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
+                .getAbsolutePath());
+        DevLog.d(getExternalCacheDir().getAbsolutePath());
+        DevLog.d(getExternalFilesDir(Environment.DIRECTORY_PODCASTS).getParentFile().getAbsolutePath());
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions, CODE_REQUIRE_PEMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CODE_REQUIRE_PEMISSION) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                ToastUtil.showShort(getApplicationContext(), "请给予存储权限!");
+                finish();
+            }
+        }
     }
 }
--- common/src/main/java/net/devwiki/common/util/FileUtil.java
+++ common/src/main/java/net/devwiki/common/util/FileUtil.java
@@ -1,8 +1,16 @@
 package net.devwiki.common.util;
 
+import android.os.Build;
+import android.os.Environment;
+import android.os.StatFs;
 import android.text.TextUtils;
 
+import net.devwiki.log.BuildConfig;
+import net.devwiki.log.DevLog;
+
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 /**
  * 文件工具类
@@ -11,12 +19,214 @@ import java.io.File;
 public class FileUtil {
 
     /**
+     * SD卡是否挂载
+     * @return true:挂载
+     */
+    public static boolean isSDMounted() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+    /**
+     * 获取SD根目录
+     * @return SD根目录且尾部带有"/"
+     */
+    public static String getSDRootPath() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator;
+    }
+
+    /**
+     * 获取下载文件夹
+     * @return 下载文件夹路径
+     */
+    public static String getDownloadDir() {
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                .getAbsolutePath() + File.separator;
+    }
+
+    /**
      * 文件是否存在
      * @param path 文件路径
      * @return true:存在,false:不存在
      */
     public static boolean isExists(String path){
-        return !TextUtils.isEmpty(path) && new File(path).exists();
+        if (!isSDMounted()) {
+            DevLog.w("sdcard is not mount!");
+            return false;
+        }
+        if (TextUtils.isEmpty(path)) {
+            DevLog.w("path is invalid!");
+            return false;
+        }
+        return new File(path).exists();
+    }
+
+    /**
+     * 创建文件或文件夹
+     * @param path 路径
+     * @return true:创建成功
+     */
+    public static boolean createFile(String path) {
+        if (!isSDMounted()) {
+            DevLog.w("sdcard is not mount!");
+            return false;
+        }
+        if (TextUtils.isEmpty(path)) {
+            DevLog.w("path is invalid!");
+            return false;
+        }
+        File file = new File(path);
+        if (file.exists()) {
+            DevLog.w("file is exists!");
+            return false;
+        }
+        if (file.isDirectory()) {
+            return file.mkdirs();
+        } else {
+            try {
+                return file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+
+    /**
+     * 删除文件
+     * @param path 文件路径
+     * @return true:删除成功
+     */
+    public static boolean deleteFile(String path) {
+        if (!isSDMounted()) {
+            DevLog.w("sdcard is not mount!");
+            return false;
+        }
+        if (TextUtils.isEmpty(path)) {
+            DevLog.w("path is invalid!");
+            return false;
+        }
+        File file = new File(path);
+        if (!file.exists()) {
+            DevLog.w("file is not exists!");
+            return false;
+        }
+        if (file.isDirectory()) {
+            DevLog.w("path is directory! please use deleteDir(String path)");
+            return false;
+        } else {
+            return file.delete();
+        }
+    }
+
+    /**
+     * 删除文件夹,此操作可能耗时,建议在子线程调用
+     * @param path 文件路径
+     * @return true:删除成功
+     */
+    public static boolean deleteDir(String path) {
+        if (!isSDMounted()) {
+            DevLog.w("sdcard is not mount!");
+            return false;
+        }
+        if (TextUtils.isEmpty(path)) {
+            DevLog.w("path is invalid!");
+            return false;
+        }
+        File file = new File(path);
+        if (!file.exists()) {
+            DevLog.w("file is not exists!");
+            return false;
+        }
+        if (file.isDirectory()) {
+            deleteDir(file);
+            return true;
+        } else {
+            DevLog.w("path is not directory!");
+            return false;
+        }
+    }
+
+    private static void deleteDir(File file) {
+        File[] files = file.listFiles();
+        if (files.length > 0) {
+            for (File child : files) {
+                if (child.isDirectory()) {
+                    deleteDir(child);
+                } else {
+                    child.delete();
+                }
+            }
+        } else {
+            file.delete();
+        }
+    }
+
+    /**
+     * 获取SD卡空间大小,单位:B
+     * @return SD大小
+     */
+    public static long getSDSizeInByte() {
+        if (isSDMounted()) {
+            StatFs statFs = new StatFs(getSDRootPath());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                return statFs.getBlockSizeLong() * statFs.getBlockCountLong();
+            } else {
+                return statFs.getBlockSize() * statFs.getBlockCount();
+            }
+        } else {
+            DevLog.w("sdcard is not mounted!");
+            return 0L;
+        }
+    }
+
+    /**
+     * 获取SD空间大小,单位:KB
+     * @return SD大小
+     */
+    public static long getSDSizeInKB() {
+        return getSDSizeInByte() / 1024;
+    }
+
+    /**
+     * 获取SD空间大小,单位:MB
+     * @return SD大小
+     */
+    public static long getSDSizeInMB() {
+        return getSDSizeInByte() / 1024 / 1024;
+    }
+
+    /**
+     * 获取SD剩余空间大小,单位:B
+     * @return SD剩余大小
+     */
+    public static long getSDFreeSizeInByte() {
+        if (isSDMounted()) {
+            StatFs statFs = new StatFs(getSDRootPath());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                return statFs.getFreeBlocksLong() * statFs.getBlockSizeLong();
+            } else {
+                return statFs.getFreeBlocks() * statFs.getBlockSize();
+            }
+        } else {
+            DevLog.w("sd is not mounted!");
+            return 0L;
+        }
+    }
+
+    /**
+     * 获取SD剩余空间大小,单位:KB
+     * @return SD剩余大小
+     */
+    public static long getSDFreeSizeInKB() {
+        return getSDFreeSizeInByte() / 1024;
+    }
+
+    /**
+     * 获取SD剩余空间大小,单位:MB
+     * @return SD剩余大小
+     */
+    public static long getSDFreeSizeInMB() {
+        return getSDFreeSizeInByte() / 1024 / 1024;
     }
 
     /**
@@ -25,11 +235,21 @@ public class FileUtil {
      * @return 占用空间大小,单位:B
      */
     public static long getSizeInByte(String path){
-        if (TextUtils.isEmpty(path)){
+        if (!isSDMounted()) {
+            DevLog.w("sd is not mounted!");
+            return 0L;
+        }
+        if (TextUtils.isEmpty(path)) {
+            DevLog.w("path is invalid!");
             return 0L;
         }
         File file = new File(path);
-        return file.getTotalSpace();
+        if (file.exists()) {
+            return file.getTotalSpace();
+        } else {
+            DevLog.w("file is not exists!");
+            return 0L;
+        }
     }
 
     /**
@@ -49,6 +269,6 @@ public class FileUtil {
      */
     public static float getSizeInMB(String path){
         long size = getSizeInByte(path);
-        return 1.0F*size/(1024*1024);
+        return 1.0F*size/1024/1024;
     }
 }
